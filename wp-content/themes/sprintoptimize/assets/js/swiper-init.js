f2c7a6601e3c87e6cf78bbb1f67e395cbac9836d
premdaryanani01 <premdaryanani@yahoo.com>
2025-02-11 17:40:58 +0530

    swiper responsive options

--- wp-content/themes/sprintoptimize/assets/js/swiper-init.js
+++ wp-content/themes/sprintoptimize/assets/js/swiper-init.js
@@ -12,8 +12,25 @@ document.addEventListener("DOMContentLoaded", function () {
           prevEl: ".swiper-button-prev",
       },
       autoplay: {
-          delay: 9000, // Auto-slide every 4 seconds
+          delay: 9000, 
           disableOnInteraction: true,
       },
+      breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1.5,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+          slidesPerView: 3.5,
+          spaceBetween: 40
+        }
+      }
   });
 });
